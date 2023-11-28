61e654b98aec68282e9f9aa82cfdda6123a1c8a9
Sergey Pavalyaev <serg-paval@inbox.ru>
2023-11-28 22:38:47 +0300

    Task 2.1

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -7,8 +7,19 @@
             //Напишите программу, которая покажет количество чётных чисел в массиве.
             int limitMin = 100;
             int limitMax = 999;
-            int size = 5;
+            int size = 7;
+            int count = 0;
             int [] array = InitArrayRand(size, limitMin, limitMax);
+            count = CountParity(array);
+            System.Console.WriteLine($"Count item of parity = {count}");
+        }
+        public static int CountParity (int [] array){
+            int count = 0;
+            foreach (int item in array)
+            {
+                if (item % 2 == 0) count++;
+            }
+            return count;
         }
         public static int [] InitArrayRand (int size, int limitMin, int limitMax){
             int [] array = new int [size];
