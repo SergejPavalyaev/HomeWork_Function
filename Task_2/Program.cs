71ef61403b86bfc70d43fb20732e9a622531a414
Sergey Pavalyaev <serg-paval@inbox.ru>
2023-11-28 22:00:34 +0300

    Task 2

---
+++ Task_2/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Task_2
+{
+    public class Program
+    {
+        public static void Main (string [] args){
+            //Задайте массив заполненный случайными трёхзначными числами.
+            //Напишите программу, которая покажет количество чётных чисел в массиве.
+            int limitMin = 100;
+            int limitMax = 999;
+            int size = 5;
+            int [] array = InitArrayRand(size, limitMin, limitMax);
+        }
+        public static int [] InitArrayRand (int size, int limitMin, int limitMax){
+            int [] array = new int [size];
+            Random rand = new Random();
+            int temp = limitMax;
+            if (limitMin > limitMax)
+            {
+                limitMax = limitMin;
+                limitMin = temp;
+            }
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = rand.Next(limitMin, limitMax + 1);
+                System.Console.Write(array[i] + " ");
+            }
+            System.Console.WriteLine();
+            return array;
+        }
+    }
+}
