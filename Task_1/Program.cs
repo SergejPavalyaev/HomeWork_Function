00abd08e31fcae8260f3bf2c8b9e5bbfe0b4189f
Sergey Pavalyaev <serg-paval@inbox.ru>
2023-11-28 21:41:18 +0300

    Task 1.2

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -7,15 +7,34 @@
             // Напишите программу, которая бесконечно запрашивает целые числа с консоли.
             // Программа завершается при вводе символа ‘q’ или при вводе числа, сумма цифр которого чётная.
             System.Console.WriteLine("Input natural number:");
-            int number = InputNumber();
-            if (number == 0) goto Found;
-            // System.Console.WriteLine(number);
-
-
+            bool parity = false;
+            while (!parity)
+            {
+                int number = InputNumber();
+                if (number == 0) goto Found;
+                parity = ParityNumbers(number);
+                if (parity) goto Found;
+                System.Console.WriteLine("Again, input natural number:");
+            }
             Found:
             System.Console.WriteLine("End program.");
         }
-
+        public static bool ParityNumbers (int number)
+        {
+            bool parity;
+            int size = Convert.ToInt32(Math.Floor(Math.Log10(number)));
+            int [] array = new int [size + 1];
+            int summ = 0;
+            for (int i = 0; i < size + 1; i++)
+            {
+                array[size - i] = number % 10;
+                number = number / 10;
+                summ += array[size - i];
+            }
+            if (summ % 2 == 0) parity = true;
+            else parity = false;
+            return parity;
+        }
         public static int InputNumber ()
         {
             string input = string.Empty;
